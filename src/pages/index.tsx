035f1b269cd96d17d5cc75d34672b6f18f7b6155
psj1997 <1176894699@qq.com>
2024-01-10 10:13:16 +0800

    Update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ const HeroComponent: React.FC<AttributesProps> = ({
               SemiMAGs is an online resource for Metagenome-Assembled Genomes (MAGs) based on PacBio HiFi sequencing technology. 
 We have curated samples from various sources, including the human gut, sheep gut, and seawater, which have been sequenced using PacBio HiFi technology and subsequently binned using SemiBin2. 
 The website showcases the quality, genomic content, and taxonomic annotations of MAGs obtained through PacBio HiFi sequencing. 
-              <br>
+              </br>
               If you1 utilize our platform, please cite：
             </p>
 
