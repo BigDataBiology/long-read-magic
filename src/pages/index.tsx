f74e10ed15500a5fae02364ff2eec9068e1748fa
psj1997 <1176894699@qq.com>
2024-01-10 09:59:09 +0800

    Update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,7 @@ const HeroComponent: React.FC<AttributesProps> = ({
             </h1>
           </div>
           <div className="pb-6 pt-2 sm:pb-10 sm:pt-6 md:pb-12">
-            <p className="font-inter gap-x-6 text-sm font-normal leading-6 text-white sm:text-[21px] md:leading-9 2xl:text-2xl">
+            <p className="font-inter gap-x-6 text-sm font-normal leading-6 text-white sm:text-[21px] md:leading-9 2xl:text-2xl white-space: pre">
               SemiMAGs is an online resource for Metagenome-Assembled Genomes (MAGs) based on PacBio HiFi sequencing technology. 
 We have curated samples from various sources, including the human gut, sheep gut, and seawater, which have been sequenced using PacBio HiFi technology and subsequently binned using SemiBin2. 
 The website showcases the quality, genomic content, and taxonomic annotations of MAGs obtained through PacBio HiFi sequencing. \n\n If you utilize our platform, please cite：
