5146cc7d757d710790f488c7778ba87845ffbd95
Anne Staff <94859949+annestaff@users.noreply.github.com>
2024-01-26 16:27:44 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -166,153 +166,154 @@ export default function Home({
         imageUrl={urlHero}
         imageAlt={altHero}
       />
-      {/*<div className="bg-backgroundColor-grey">
-        <div className="max-x-[400px]:pb-1 relative grid max-w-[1920px] justify-center bg-backgroundColor-grey pb-36 pt-14 md:grid-cols-2 md:pb-[112px] md:pr-[108px] md:pt-[120px] 2xl:mx-auto">
-          <Image
-            src={imageAbout}
-            alt="image cells"
-            className="blur-2 left-0 top-0 opacity-50 md:absolute xl:opacity-100 "
-          />
-          <div className="absolute top-6 mx-4 sm:top-8 md:relative md:top-0 md:ml-[54px] lg:ml-[108px]">
-            <h2
-              className="transform text-4xl font-medium text-textColor-blue duration-700 ease-in-out lg:pl-[54px] lg:text-[52px] "
-              id="sticky1"
-              style={{ width: width1 }}
-            >
-              {titleAbout}
-            </h2>
-          </div>
-          <div className="absolute top-28 mx-4 flex flex-col items-center gap-6 sm:top-48 md:relative md:top-0 md:gap-10 xl:gap-[120px] 2xl:gap-16">
-            {descriptionAbout.map((content: ItemProps, index) => (
-              <div key={`${titleAbout}-${index}`} className="fadeIn-on-scrool">
-                <p className=" text-base leading-6 text-textColor-blue sm:text-lg  md:leading-9 lg:text-[21px]">
-                  {content.description}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="relative flex h-[420px] min-h-max w-full max-w-[1920px] items-center justify-center bg-backgroundColor-blue sm:h-full 2xl:mx-auto">
-          <div className="relative flex items-center justify-center md:px-10 2xl:px-0">
-            <Image
-              src={urlImageSection2}
-              alt={altImageSection2}
-              sizes="100vw"
-              width={800}
-              height={504}
-              className="backdrop-blur-[2px] backdrop-filter"
-            />
-            <div className="absolute mx-auto flex max-w-[80%] items-center justify-center sm:max-w-[70%]">
-              <div className="rounded bg-backgroundColor-greylight p-2 text-center opacity-80 sm:px-6 sm:py-10">
-                <h2 className="pb-2 text-4xl font-medium text-textColor-blue sm:pb-7 lg:text-[52px]">
-                  {titleSection2}
-                </h2>
-                <p className="leading-1 text-base text-textColor-blue sm:text-lg md:leading-9 lg:text-[21px]">
-                  {descriptionSection2}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid max-w-[1920px] bg-backgroundColor-grey pb-[93px] pt-14 md:grid-cols-2 md:pr-[108px] md:pt-[120px] 2xl:mx-auto">
-          <div className=" mx-4 pb-8  md:pl-[54px] md:pr-10 lg:pl-[108px] lg:text-[52px] 2xl:pr-10">
-            <h2
-              className="ease-out-in transform text-4xl font-medium text-textColor-blue duration-100 lg:text-[52px]"
-              id="sticky2"
-              style={{ width: width2 }}
-            >
-              {titleSection3}
-            </h2>
-          </div>
-          <div className="mx-4 flex flex-col items-center md:gap-10 xl:gap-36 2xl:gap-16">
-            {descriptionSection3.map((content: ItemProps, index) => (
-              <div
-                key={`${titleSection3}-${index}`}
-                className="fadeIn-on-scrool"
-              >
-                <p className="text-base leading-9 text-textColor-blue sm:text-lg lg:text-[21px]">
-                  {content.description}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div
-          className="relative max-w-[1920px] px-6 pb-28 pt-20 2xl:mx-auto"
-          id="sticky2-next"
-        >
-          <div className="absolute inset-0 ">
-            <Image
-              src={urlImageSection4}
-              alt={altImageSection4}
-              fill
-              className="object-cover object-center"
-            />
-            <div
-              className="h-100% absolute inset-0 z-0"
-              style={{
-                background:
-                  'linear-gradient(91deg, #001135 0%, rgba(0, 17, 53, 0.00) 100%)',
-                backdropFilter: 'blur(2px)',
-              }}
-            ></div>
-          </div>
-          <div className="relative flex h-full flex-col gap-6 lg:pl-[54px] lg:pr-[54px]">
-            <div className="md:pl-[54px]">
-              <h2 className="pb-6 text-4xl font-medium text-white md:w-1/2 lg:text-[52px]">
-                {titleSection4}
-              </h2>
-              <p className="text-base leading-9 text-white sm:text-lg md:w-1/2 lg:text-[21px]">
-                {descriptionSection4}
-              </p>
-            </div>
-            <div className="mb-4">
-              <Tabs>
-                {tabs.map((tab: TabProps) => (
-                  <Tab key={tab.name} label={tab.name}>
-                    <div className="backdrop-blur-15 grid rounded bg-opacity-50 bg-gradient-4 p-6 sm:p-[54px] lg:grid-cols-2 lg:gap-[100px]">
-                      <div>
-                        <h2 className="text-2xl font-medium leading-9 text-textColor-blue">
-                          {tab.name}
-                        </h2>
-                        {tab.content.map((item, index) => (
-                          <div
-                            key={`${tab.name}-${index}`}
-                            className="pb-5 pt-6 leading-9"
-                          >
-                            <p className="text-lg text-textColor-blue">
-                              {item.paragraph}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
-                      <div>
-                        <h2 className="text-2xl font-medium leading-9 text-textColor-blue">
-                          Workflow
-                        </h2>
-                        {tab.workflow.map((item, index) => (
-                          <div
-                            key={`workflow-${index}`}
-                            className="pb-5 pt-6 leading-9 text-textColor-blue"
-                          >
-                            <p className="text-lg">{item.paragraph}</p>
-                          </div>
-                        ))}
-                        <Button
-                          text={nameLink}
-                          logo={logoGithub}
-                          logoHover={logoGithubHover}
-                          url={tab.link}
-                          aria-label="explore workflow"
-                        />
-                      </div>
-                    </div>
-                  </Tab>
-                ))}
-              </Tabs>
-            </div>
-          </div>
-        </div>*/}
+      
+        {/* <div className="bg-backgroundColor-grey"> */}
+        {/*   <div className="max-x-[400px]:pb-1 relative grid max-w-[1920px] justify-center bg-backgroundColor-grey pb-36 pt-14 md:grid-cols-2 md:pb-[112px] md:pr-[108px] md:pt-[120px] 2xl:mx-auto"> */}
+        {/*     <Image */}
+        {/*       src={imageAbout} */}
+        {/*       alt="image cells" */}
+        {/*       className="blur-2 left-0 top-0 opacity-50 md:absolute xl:opacity-100 " */}
+        {/*     /> */}
+        {/*     <div className="absolute top-6 mx-4 sm:top-8 md:relative md:top-0 md:ml-[54px] lg:ml-[108px]"> */}
+        {/*       <h2 */}
+        {/*         className="transform text-4xl font-medium text-textColor-blue duration-700 ease-in-out lg:pl-[54px] lg:text-[52px] " */}
+        {/*         id="sticky1" */}
+        {/*         style={{ width: width1 }} */}
+        {/*       > */}
+        {/*         {titleAbout} */}
+        {/*       </h2> */}
+        {/*     </div> */}
+        {/*     <div className="absolute top-28 mx-4 flex flex-col items-center gap-6 sm:top-48 md:relative md:top-0 md:gap-10 xl:gap-[120px] 2xl:gap-16"> */}
+        {/*       {descriptionAbout.map((content: ItemProps, index) => ( */}
+        {/*         <div key={`${titleAbout}-${index}`} className="fadeIn-on-scrool"> */}
+        {/*           <p className=" text-base leading-6 text-textColor-blue sm:text-lg  md:leading-9 lg:text-[21px]"> */}
+        {/*             {content.description} */}
+        {/*           </p> */}
+        {/*         </div> */}
+        {/*       ))} */}
+        {/*     </div> */}
+        {/*   </div> */}
+        {/*   <div className="relative flex h-[420px] min-h-max w-full max-w-[1920px] items-center justify-center bg-backgroundColor-blue sm:h-full 2xl:mx-auto"> */}
+        {/*     <div className="relative flex items-center justify-center md:px-10 2xl:px-0"> */}
+        {/*       <Image */}
+        {/*         src={urlImageSection2} */}
+        {/*         alt={altImageSection2} */}
+        {/*         sizes="100vw" */}
+        {/*         width={800} */}
+        {/*         height={504} */}
+        {/*         className="backdrop-blur-[2px] backdrop-filter" */}
+        {/*       /> */}
+        {/*       <div className="absolute mx-auto flex max-w-[80%] items-center justify-center sm:max-w-[70%]"> */}
+        {/*         <div className="rounded bg-backgroundColor-greylight p-2 text-center opacity-80 sm:px-6 sm:py-10"> */}
+        {/*           <h2 className="pb-2 text-4xl font-medium text-textColor-blue sm:pb-7 lg:text-[52px]"> */}
+        {/*             {titleSection2} */}
+        {/*           </h2> */}
+        {/*           <p className="leading-1 text-base text-textColor-blue sm:text-lg md:leading-9 lg:text-[21px]"> */}
+        {/*             {descriptionSection2} */}
+        {/*           </p> */}
+        {/*         </div> */}
+        {/*       </div> */}
+        {/*     </div> */}
+        {/*   </div> */}
+        {/*   <div className="grid max-w-[1920px] bg-backgroundColor-grey pb-[93px] pt-14 md:grid-cols-2 md:pr-[108px] md:pt-[120px] 2xl:mx-auto"> */}
+        {/*     <div className=" mx-4 pb-8  md:pl-[54px] md:pr-10 lg:pl-[108px] lg:text-[52px] 2xl:pr-10"> */}
+        {/*       <h2 */}
+        {/*         className="ease-out-in transform text-4xl font-medium text-textColor-blue duration-100 lg:text-[52px]" */}
+        {/*         id="sticky2" */}
+        {/*         style={{ width: width2 }} */}
+        {/*       > */}
+        {/*         {titleSection3} */}
+        {/*       </h2> */}
+        {/*     </div> */}
+        {/*     <div className="mx-4 flex flex-col items-center md:gap-10 xl:gap-36 2xl:gap-16"> */}
+        {/*       {descriptionSection3.map((content: ItemProps, index) => ( */}
+        {/*         <div */}
+        {/*           key={`${titleSection3}-${index}`} */}
+        {/*           className="fadeIn-on-scrool" */}
+        {/*         > */}
+        {/*           <p className="text-base leading-9 text-textColor-blue sm:text-lg lg:text-[21px]"> */}
+        {/*             {content.description} */}
+        {/*           </p> */}
+        {/*         </div> */}
+        {/*       ))} */}
+        {/*     </div> */}
+        {/*   </div> */}
+        {/*   <div */}
+        {/*     className="relative max-w-[1920px] px-6 pb-28 pt-20 2xl:mx-auto" */}
+        {/*     id="sticky2-next" */}
+        {/*   > */}
+        {/*     <div className="absolute inset-0 "> */}
+        {/*       <Image */}
+        {/*         src={urlImageSection4} */}
+        {/*         alt={altImageSection4} */}
+        {/*         fill */}
+        {/*         className="object-cover object-center" */}
+        {/*       /> */}
+        {/*       <div */}
+        {/*         className="h-100% absolute inset-0 z-0" */}
+        {/*         style={{ */}
+        {/*           background: */}
+        {/*             'linear-gradient(91deg, #001135 0%, rgba(0, 17, 53, 0.00) 100%)', */}
+        {/*           backdropFilter: 'blur(2px)', */}
+        {/*         }} */}
+        {/*       ></div> */}
+        {/*     </div> */}
+        {/*     <div className="relative flex h-full flex-col gap-6 lg:pl-[54px] lg:pr-[54px]"> */}
+        {/*       <div className="md:pl-[54px]"> */}
+        {/*         <h2 className="pb-6 text-4xl font-medium text-white md:w-1/2 lg:text-[52px]"> */}
+        {/*           {titleSection4} */}
+        {/*         </h2> */}
+        {/*         <p className="text-base leading-9 text-white sm:text-lg md:w-1/2 lg:text-[21px]"> */}
+        {/*           {descriptionSection4} */}
+        {/*         </p> */}
+        {/*       </div> */}
+        {/*       <div className="mb-4"> */}
+        {/*         <Tabs> */}
+        {/*           {tabs.map((tab: TabProps) => ( */}
+        {/*             <Tab key={tab.name} label={tab.name}> */}
+        {/*               <div className="backdrop-blur-15 grid rounded bg-opacity-50 bg-gradient-4 p-6 sm:p-[54px] lg:grid-cols-2 lg:gap-[100px]"> */}
+        {/*                 <div> */}
+        {/*                   <h2 className="text-2xl font-medium leading-9 text-textColor-blue"> */}
+        {/*                     {tab.name} */}
+        {/*                   </h2> */}
+        {/*                   {tab.content.map((item, index) => ( */}
+        {/*                     <div */}
+        {/*                       key={`${tab.name}-${index}`} */}
+        {/*                       className="pb-5 pt-6 leading-9" */}
+        {/*                     > */}
+        {/*                       <p className="text-lg text-textColor-blue"> */}
+        {/*                         {item.paragraph} */}
+        {/*                       </p> */}
+        {/*                     </div> */}
+        {/*                   ))} */}
+        {/*                 </div> */}
+        {/*                 <div> */}
+        {/*                   <h2 className="text-2xl font-medium leading-9 text-textColor-blue"> */}
+        {/*                     Workflow */}
+        {/*                   </h2> */}
+        {/*                   {tab.workflow.map((item, index) => ( */}
+        {/*                     <div */}
+        {/*                       key={`workflow-${index}`} */}
+        {/*                       className="pb-5 pt-6 leading-9 text-textColor-blue" */}
+        {/*                     > */}
+        {/*                       <p className="text-lg">{item.paragraph}</p> */}
+        {/*                     </div> */}
+        {/*                   ))} */}
+        {/*                   <Button */}
+        {/*                     text={nameLink} */}
+        {/*                     logo={logoGithub} */}
+        {/*                     logoHover={logoGithubHover} */}
+        {/*                     url={tab.link} */}
+        {/*                     aria-label="explore workflow" */}
+        {/*                   /> */}
+        {/*                 </div> */}
+        {/*               </div> */}
+        {/*             </Tab> */}
+        {/*           ))} */}
+        {/*         </Tabs> */}
+        {/*       </div> */}
+        {/*     </div> */}
+        {/*   </div> */}
 
         {/*<div className="px-4 py-[90px] md:px-[54px] xl:px-[108px]">*/}
         {/*  <ContactForm*/}
